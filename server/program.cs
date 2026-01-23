07caf947dadb766be24b384012612e11b884ea26
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-01-23 09:29:07 +0300

    Adding group and date

--- server/program.cs
+++ server/program.cs
@@ -1,2 +1,4 @@
 ﻿Console.WriteLine("Привет меня зовут Гвоздева Вероника Александровна");
 Console.WriteLine("Привет меня зовут Деушев Тимур Тимурович");
+Console.WriteLine("ИСП-231");
+Console.WriteLine($"{DateTime.Now}, {DateTime.Now.DayOfWeek}");
