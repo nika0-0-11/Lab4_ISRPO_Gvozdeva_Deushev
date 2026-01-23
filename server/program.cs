e144346e38c7e27e84a77b7deac8321f97028669
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-01-23 09:50:00 +0300

    Adding menu and choose logic

--- server/program.cs
+++ server/program.cs
@@ -1,4 +1,30 @@
-﻿Console.WriteLine("Привет меня зовут Гвоздева Вероника Александровна");
+﻿/*Console.WriteLine("Привет меня зовут Гвоздева Вероника Александровна");
 Console.WriteLine("Привет меня зовут Деушев Тимур Тимурович");
 Console.WriteLine("ИСП-231");
-Console.WriteLine($"{DateTime.Now}, {DateTime.Now.DayOfWeek}");
+Console.WriteLine($"{DateTime.Now}, {DateTime.Now:D}");*/
+
+while(true)
+{
+    Console.WriteLine("1 - Показать ФИО \n2 - Показать группу \n3 - Показать дату \n4 - Выход");
+    Console.Write("Введите номер: ");
+
+    switch (Convert.ToInt32(Console.ReadLine()))
+    {
+        case 1:
+            Console.WriteLine("Гвоздева Вероника Александровна");
+            Console.WriteLine("Деушев Тимур Тимурович \n");
+            break;
+
+        case 2:
+            Console.WriteLine("ИСП-231 \n");
+            break;
+
+        case 3:
+            Console.WriteLine($"{DateTime.Now:D} \n");
+            break;
+
+        case 4:
+            Console.WriteLine("Выход из программы \n");
+            return;
+    }
+}
